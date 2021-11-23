4917f8795f1cfd3063765af752e919987ac63e6b
Nooryoku Rafshanjany <nooryoku12331@gmail.com>
2021-11-23 11:48:27 +0700

    HTTP POST is working

---
+++ src/main/java/guru/springframework/msscbrewery/services/BeerServiceImpl.java
@@ -0,0 +1,19 @@
+package guru.springframework.msscbrewery.services;
+
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+import guru.springframework.msscbrewery.web.model.BeerDto;
+
+@Service
+public class BeerServiceImpl implements BeerService{
+    @Override
+    public BeerDto getBeerById(UUID beerId) {
+        return BeerDto.builder().id(UUID.randomUUID())
+            .beerName("Galaxy Cat")
+            .beerStyle("Pale Ale")
+            .build();
+    }
+    
+}
